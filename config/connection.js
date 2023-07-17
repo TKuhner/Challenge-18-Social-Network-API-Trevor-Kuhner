e3bbc25c949cfac0d1fd122291ed607a2b9a00ef
TKuhner <trevorkuhner29@gmail.com>
2023-07-16 19:42:15 -0500

    adjusted connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,8 @@ const mongoose = require('mongoose');
 
 const connection = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNet';
 
-mongoose.connect(connection, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(connection);
 
 const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
 
 module.exports = db;
